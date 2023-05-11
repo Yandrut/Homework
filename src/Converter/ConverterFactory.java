2b606fc64b83a3b53c05201a86dc4063f343dc45
Yandrut <nickolayko@yahoo.com>
2023-05-11 17:30:21 +0300

    Initial Commit

--- src/Converter/ConverterFactory.java
+++ src/Converter/ConverterFactory.java
@@ -2,19 +2,15 @@ package Converter;
 
 public class ConverterFactory {
 
-   public static Converter getConverter (char option) {
-       if (option == 'c' || option == 'C') {
+   public static Converter getConverter (String option) {
+       if (option.equals("c") || option.equals("C")) {
            return new CelsiusToFahrenheit();
-       } else if (option == 'f' || option == 'F')
+       } else if (option.equals("f") || option.equals("F")) {
            return new FahrenheitToCelsius();
-       else {
+       } else if (option.equals("currency")) {
+           return new CurrencyConverter();
+       } else {
           throw new IllegalArgumentException("Converter was not found");
        }
    }
-        public static Converter getConverter (String option) {
-            if (option.equals("currency")) {
-                return new CurrencyConverter();
-            }
-            throw new IllegalArgumentException("Converter was not found");
-        }
     }
--- src/Converter/Main.java
+++ src/Converter/Main.java
@@ -12,52 +12,28 @@ public class Main {
 
             System.out.println();
             System.out.println("--- Choose action ---");
-            System.out.println("1: Convert fahrenheit to celsius");
-            System.out.println("2: Convert celsius to fahrenheit");
-            System.out.println("3: Convert Dollars to Hryvnias");
-            System.out.println("0: Exit program");
+            System.out.println("f : Convert fahrenheit to celsius");
+            System.out.println("c : Convert celsius to fahrenheit");
+            System.out.println("currency : Convert Dollars to Hryvnias");
+            System.out.println("0 : exit program");
             System.out.println();
             System.out.println("Enter action: ");
             String input = in.nextLine();
 
-
-
-            switch (input) {
-                case "0":
+            try {
+                if (input.equals("0")) {
                     in.close();
                     return;
-                case "1":
-                   try {
-                       System.out.print("Enter number to convert : ");
-                       toConvert = in.nextInt();
-                       converter = ConverterFactory.getConverter('f');
-                       System.out.println(toConvert + " Fahrenheit In Celsius is: " + converter.convert(toConvert));
-                   } catch (IllegalArgumentException e) {
-                       System.out.println(e.getMessage());
-                   }
-                   break;
-                case "2":
-                    try {
-                        System.out.print("Enter number to convert : ");
-                        toConvert = in.nextInt();
-                        converter = ConverterFactory.getConverter('c');
-                        System.out.println(toConvert + " Celsius In Fahrenheit is: " + converter.convert(toConvert));
-                    } catch (IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case "3":
-                    try {
-                        System.out.print("Enter amount of Dollars you have: ");
-                        toConvert = in.nextInt();
-                        converter = ConverterFactory.getConverter("currency");
-                        System.out.println(toConvert + " In Hryvnias is: " + converter.convert(toConvert));
-                    } catch (IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
+                }
+                converter = ConverterFactory.getConverter(input);
+                System.out.println("Enter number to convert: ");
+                toConvert = in.nextInt();
+                System.out.println("Result: " + converter.convert(toConvert));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
         }
+
     }
 }
 
