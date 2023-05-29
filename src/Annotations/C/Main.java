db68436912c49438900421e714454b2815458a24
Yandrut <nickolayko@yahoo.com>
2023-05-29 16:09:46 +0300

    Homework

--- src/Annotations/C/Main.java
+++ src/Annotations/C/Main.java
@@ -1,9 +1,9 @@
 package Annotations.C;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 
 public class Main {
-   
     public static void main(String[] args) {
         TestClass methods = new TestClass("");
         Class<TestClass> testClassObj = TestClass.class;
@@ -12,17 +12,19 @@ public class Main {
             Constructor constructor = testClassObj.getConstructor(String.class);
             Object instance = constructor.newInstance("String");
             TestClass newInstanceObject = (TestClass) instance;
-            Method[] methodsArray = methods.getClass().getMethods();
-
-            for (Method method : methodsArray) {
-                if (method.getParameterCount() == 0 && method.getReturnType().equals(void.class)) {
-                    method.invoke(newInstanceObject);
-                } else {
-                    System.out.println("Method error: " + method.getName() + ". Return type: " + method.getReturnType() + ". Number of parameters: " + method.getParameterCount());
-                }
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
+            Method[] methodsArray = methods.getClass().getDeclaredMethods();
+                for (Method method : methodsArray) {
+                    Annotation annotation = method.getAnnotation(Test.class);
+                            if (annotation != null &&
+                                method.getParameterCount() == 0 &&
+                                method.getReturnType().equals(void.class)) {
+                                     method.invoke(newInstanceObject);
+                            }   else if (annotation != null) {
+                                    System.out.println("Method error: " + method.getName() + ". Return type: " + method.getReturnType() + ". Number of parameters: " + method.getParameterCount());
+                        }
+                    }
+                } catch (Exception exception) {
+                        exception.printStackTrace();
         }
     }
 }
--- src/Annotations/C/TestClass.java
+++ src/Annotations/C/TestClass.java
@@ -6,11 +6,19 @@ public class TestClass {
     public void secondTest () {
         System.out.println("testParameter is working");
     }
+    @Test
     public int[] testInt(int ... ints) {
         return ints;
     }
     @Test
     public void myFirstTest() {
         System.out.println("Test is working");
+    }
+    @Test
+    public void nothing (int [] nums) {
+        System.out.println("Takes integer array");
+    }
+    public void testWithNoAnnotation () {
+        System.out.println(this.toString());
     }
 }
