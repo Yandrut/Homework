23b0aedf9798ebb9396ab5ea12eebe67c9b206ab
Yandrut <nickolayko@yahoo.com>
2023-06-02 18:02:20 +0300

    Homework

--- src/Enum/A/Days.java
+++ src/Enum/A/Days.java
@@ -15,23 +15,23 @@ public enum Days {
         throw new IllegalArgumentException(day + " is not a weekday");
     }
 
-//    public static String dayOff(String day) {
-//        if (day == null || day.isEmpty()) throw new IllegalArgumentException("Day cannot be null");
-//
-//        if (day.equalsIgnoreCase(String.valueOf(Days.SUNDAY)) ||
-//                day.equalsIgnoreCase(String.valueOf(Days.SATURDAY))) {
-//            return day + " is a day off!";
-//        }
-//        Days[] days = Days.values();
-//        String[] stringDays = new String[days.length];
-//
-//        for (int i = 0; i < days.length; i++) {
-//            stringDays[i] = String.valueOf(days[i]);
-//
-//            if (day.equalsIgnoreCase(stringDays[i])) {
-//                return day + " is a work day";
-//            }
-//        }
-//        throw new IllegalArgumentException(day + " is not a valid day of the week");
-//    }
+   public String dayOff(String day) {
+       if (day == null || day.isEmpty()) throw new IllegalArgumentException("Day cannot be null");
+
+       if (day.equalsIgnoreCase(String.valueOf(Days.SUNDAY)) ||
+                day.equalsIgnoreCase(String.valueOf(Days.SATURDAY))) {
+            return day + " is a day off!";
+        }
+        Days[] days = Days.values();
+        String[] stringDays = new String[days.length];
+
+        for (int i = 0; i < days.length; i++) {
+            stringDays[i] = String.valueOf(days[i]);
+
+            if (day.equalsIgnoreCase(stringDays[i])) {
+                return day + " is a work day";
+            }
+        }
+        throw new IllegalArgumentException(day + " is not a valid day of the week");
+    }
 }
