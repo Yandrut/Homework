34fcad426c2ef26359b6afb87fa99e67538c37c8
Yandrut <nickolayko@yahoo.com>
2023-06-24 12:42:09 +0300

    updated MyArrayList

--- src/MyList/MyArrayListTest.java
+++ src/MyList/MyArrayListTest.java
@@ -5,18 +5,13 @@ public class MyArrayListTest {
         long start = System.nanoTime();
 
         MyArrayList<String> list = new MyArrayList<String>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 20; i++) {
             list.add("Object" + i);
-        }
-        for (int i = 0; i < list.size(); i++) {
-            list.remove(i);
         }
         long duration = (System.nanoTime() - start)/1000000;
         System.out.println("Duration: " + duration + " ms.");
         for (String s : list) {
-            String result = "";
-            result += s;
-            System.out.println(result);
+            System.out.print(s + " ");
         }
     }
 }
